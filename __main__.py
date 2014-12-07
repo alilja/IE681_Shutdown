b4dc8523f9d402d845b22ea8815c79f1627192f1
Andrew Lilja <andrewlilja@gmail.com>
2014-12-07 12:27:15 -0600

    messages implemented

--- __main__.py
+++ __main__.py
@@ -1,33 +1,60 @@
 import simpy
 
 from models import Weather, Employee
+from messages import BroadcastPipe
 
+
+def message_generator(name, env, out_pipe):
+    """A process which randomly generates messages."""
+    while True:
+        # wait for next transmission
+        yield env.timeout(1)
+
+        # messages are time stamped to later check if the consumer was
+        # late getting them.  Note, using event.triggered to do this may
+        # result in failure due to FIFO nature of simulation yields.
+        # (i.e. if at the same env.now, message_generator puts a message
+        # in the pipe first and then message_consumer gets from pipe,
+        # the event.triggered will be True in the other order it will be
+        # False
+        msg = (env.now, '%s says hello at %d' % (name, env.now + 8))
+        out_pipe.put(msg)
 
 env = simpy.Environment()
-weather = Weather(env, 1, 5, 1)
+weather = Weather(
+    env=env,
+    time=2,
+    distance=20,
+    intensity=6,
+)
 
+bc_pipe = BroadcastPipe(env)
 employees = [
     Employee(
         env=env,
         weather=weather,
         distance=30.0,
-        speed=60,
-        kind=Employee.kind.staff
+        kind=Employee.kind.staff,
+        pipe=bc_pipe.get_output_conn(),
     ),
     Employee(
         env=env,
         weather=weather,
         distance=2.0,
         speed=60,
-        kind=Employee.kind.student_worker
+        kind=Employee.kind.student_worker,
+        pipe=bc_pipe.get_output_conn(),
     ),
     Employee(
         env=env,
         weather=weather,
         distance=0.5,
         speed=60,
-        kind=Employee.kind.student_worker
+        kind=Employee.kind.student_worker,
+        pipe=bc_pipe.get_output_conn(),
     ),
 ]
+
+env.process(message_generator('Generator A', env, bc_pipe))
 
 env.run(until=24)
--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+import simpy
 from enum import Enum
 
 CYRIDE = True
@@ -9,13 +10,8 @@ class Weather(object):
     Args:
         time: How long until the weather arrives
         distance: How far away the weather begins
-        intensity: Weather.intensity enum of how intense the weather
-            should be."""
-    class intensity(Enum):
-        clear = 0
-        bad = 1
-        really_bad = 2
-        apocalyptic = 6
+        intensity: How intense the weather should be. Higher is
+            more intense."""
 
     def __init__(self, env, time, distance, intensity):
         self.env = env
@@ -46,12 +42,13 @@ class Employee(object):
 
     _counter = 0
 
-    def __init__(self, env, weather, distance, speed=45, kind=0):
+    def __init__(self, env, weather, distance, pipe, speed=45, kind=0):
         self.env = env
         self.speed = speed
         self.distance = distance
         self.weather = weather
         self.kind = kind
+        self.pipe = pipe
 
         self.id = Employee._counter
         Employee._counter += 1
@@ -64,11 +61,14 @@ class Employee(object):
     def run(self):
         while True:
             print "{0}: Waking up at {1}.".format(self.id, self.env.now + 8 % 24)
+            yield self.env.process(self.check_for_messages(self.pipe))
             yield self.env.timeout(1)
 
             print "{0}: Checking weather.".format(self.id)
             # the weather will get to work faster than they will
-            if self.weather.distance / self.weather.speed < self.distance / self.speed:
+            print self.weather.distance / self.weather.speed
+            print self.distance / self.speed
+            if (self.weather.distance / self.weather.speed) / self.weather.intensity < self.distance / self.speed:
                 print "{0}: Staying home.".format(self.id)
                 break
 
@@ -82,16 +82,29 @@ class Employee(object):
 
             self.location = Employee.location.work
             print "{0}: Arrived at work at {1}.".format(self.id, self.env.now + 8 % 24)
-            yield self.env.timeout(8)
+            for i in range(8):
+                yield self.env.process(self.work(1))
+                print "{0}: Checking messages.".format(self.id)
+                yield self.env.process(self.check_for_messages(self.pipe))
 
             print "{0}: Leaving work at {1}.".format(self.id, self.env.now + 8 % 24)
             yield self.env.process(self.travel(self.distance))
 
             self.location = Employee.location.home
             print "{0}: Arrived at home at {1}".format(self.id, self.env.now + 8 % 24)
-            yield self.env.timeout(24 - ((self.distance / self.speed) * 2 + 9))
+            yield self.env.timeout(24 - self.env.now)
             print "{0}: Slept until {1}.".format(self.id, self.env.now + 8 % 24)
 
     def travel(self, distance):
         self.location = Employee.location.traveling
         yield self.env.timeout(distance / self.speed)
+
+    def work(self, duration):
+        yield self.env.timeout(duration)
+
+    def check_for_messages(self, in_pipe):
+        msg = yield in_pipe.get()
+        if msg[0] < self.env.now:
+            print "Late message received."
+
+        print "Message: {0}".format(msg[1])
